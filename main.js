38439a04985bdb98392ad345d7787d42627e6d98
DIEGOFLOREZBELTRAN <diflorez@misena.edu.co>
2021-03-29 18:01:12 -0500

    MOdificacion en el archivo main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let nombre;
 let edad;
 nombre = prompt(`ingrese su nombre`);
 edad = prompt(`ingrese su edad`);
-console.log(nombre);
+console.log(nombre); 
 console.log(typeof(nombre));
 console.log(edad);
 console.log(typeof(edad));
